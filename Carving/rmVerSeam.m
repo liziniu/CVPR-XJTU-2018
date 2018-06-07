288d8422440da8e61d31142a1494aa0e6065cfde
Li Ziniu <374387855@qq.com>
2018-06-07 08:38:56 +0800

    Update rmVerSeam.m

--- Carving/rmVerSeam.m
+++ Carving/rmVerSeam.m
@@ -1,4 +1,4 @@
-function [Ix, Ex] = rmVerSeam(I, Mx, Tbx)
+function [Ix, Ex, rmask] = rmVerSeam(I, Mx, Tbx, mask)
 % I is the image. Note that I could be color or grayscale image.
 % Mx is the cumulative minimum energy map along vertical direction.
 % Tbx is the backtrack table along vertical direction.
@@ -8,12 +8,14 @@ function [Ix, Ex] = rmVerSeam(I, Mx, Tbx)
 [ny, nx, nz] = size(I);
 rmIdx = zeros(ny, 1);
 Ix = uint8(zeros(ny, nx-1, nz));
-
+rmask = zeros(ny, nx-1);
 %% Add your code here
 Ex = min(Mx(end,:));
 index = find(Mx(end, :) == Ex);
 for j = ny:-1:2
     Ix(j, 1:index-1, :) = I(j, 1:index-1, :);
+    rmask(j, 1:index-1) = mask(j, 1:index-1);
     Ix(j, index:end, :) = I(j,index+1:end, :);
+    rmask(j, index:end) = mask(j, index+1:end);
     index = Tbx(j, index);
 end

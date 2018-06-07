7c14841d3e6d9512fd58258573bbb3e6c6ecec01
Li Ziniu <374387855@qq.com>
2018-06-07 08:38:07 +0800

    Update genEngMap.m

--- Carving/genEngMap.m
+++ Carving/genEngMap.m
@@ -1,6 +1,16 @@
-function e = genEngMap(I)
+function e = genEngMap(I,mask, type)
 % I is an image. I could be of color or grayscale.
 % e is the energy map of n-by-m matrix.
+
+if(~exist('type', 'var'))
+    type = 'NC';
+end
+
+if(~exist('mask', 'var'))
+    [h, w] = size(I);
+    mask = zeros(h, w);
+end
+
 
 if ndims(I) == 3
     % Assume the image fed in is a 3-channel RGB color image
@@ -12,5 +22,9 @@ end
 
 [gx, gy] = gradient(Ig);
 e = abs(gx) + abs(gy);
-
+if strcmp(type, 'remove')
+    e(logical(mask)) = -1;
+end
+if strcmp(type, 'remain')
+    e(logical(mask)) = inf;
 end
